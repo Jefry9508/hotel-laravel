bc374b741b01125e04236aa0ebba46013222e039
Jefry9508 <jeffrykr95@gmail.com>
2018-05-14 17:50:26 -0500

    Update

--- hotelLaravelProject/resources/views/layouts/app.blade.php
+++ hotelLaravelProject/resources/views/layouts/app.blade.php
@@ -122,36 +122,6 @@
         }
     </style>
     @yield('css')
-    <script>
-        function buscar() {
-            var input, filter, table, tr, td, i;
-            input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
-            table = document.getElementById("myTable");
-            tr = table.getElementsByTagName("tr");
-            option=document.getElementById("p1").value;
-            for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[option];
-                if (td) {
-                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = "";
-                    } else {
-                        tr[i].style.display = "none";
-                    }
-                }
-            }
-        }
-        function onChange(value) {
-            input=document.getElementById("myInput");
-            if(value==="0"){
-                input.placeholder="Escribe nombre del hotel...";
-            }
-            if(value==="3"){
-                input.placeholder="Escribe nombre de la ciudad...";
-            }
-        }
-    </script>
-
 </head>
 
 <body class="skin-blue sidebar-mini">
